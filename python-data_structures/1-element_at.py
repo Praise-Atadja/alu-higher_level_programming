537a703d7d4380fbe1f08e63633ad3ebd1bc7d00
Praise Atadja <p.atadja@alustudent.com>
2022-07-14 00:37:53 -0700

    retrieves an element from a list

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,6 @@
+def element_at(my_list, idx):
+    for idx in my_list:
+        if idx == negative:
+            print("None")
+            if idx > number of elements in my_list:
+                print("None")
