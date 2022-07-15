1d8fb0f87c31bea945deaa34e01f008d6e4097a7
Praise Atadja <p.atadja@alustudent.com>
2022-07-15 07:08:23 -0700

    retrieves an element from a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,9 +3,9 @@ def element_at(my_list, idx):
     if idx < 0:
         return (None)
 
-    range = len(my_list)
+    length = len(my_list)
 
-    if idx > range  - 1:
+    if idx > length - 1:
         return (None)
 
     return(my_list[idx])
