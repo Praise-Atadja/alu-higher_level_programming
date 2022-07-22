66e54aba3358b0437d7b9179b4d566ae27907e14
Praise Atadja <p.atadja@alustudent.com>
2022-07-22 02:48:53 -0700

    Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-num = 0
-for i in range x
-try:
-    num = 1
-    print("{}.format(mylist[i]")
-except IndexError
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    for i in range x:
+        try:
+            print("{}".format(mylist[i], end="")
+            num += 1
+        except IndexError:
+            break
+        
+        print("")
+        return(num)
