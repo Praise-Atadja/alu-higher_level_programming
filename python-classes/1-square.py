2155f057fb17dfc3765597e332f18f586555b337
Praise-Atadja <106454335+Praise-Atadja@users.noreply.github.com>
2022-07-22 23:38:00 +0200

    review

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,4 +5,4 @@
 class Square:
     """Initializing the size"""
     def __init__(self, size):
-        self.__size = size  #the __ is used to declare a  private attribute
+        self.__size = size
