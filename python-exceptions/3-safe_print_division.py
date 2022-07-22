220634dafb27aa723e118b50d3e484335be8d212
Praise Atadja <p.atadja@alustudent.com>
2022-07-22 05:58:18 -0700

    Integers division with debug

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        quotient = a/b
+        quotient = safe_print_division(a, b)
     except (ZeroDivisionError, ValueError, RuntimeError, TypeError):
         quotient = None
     finally:
