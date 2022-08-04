c6fb4c2b8a2937a19fafa1d335d7d895aaa81202
Praise Atadja <p.atadja@alustudent.com>
2022-08-04 00:58:21 -0700

    List databases

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,4 +1,4 @@
-"""Write a script that lists all databases of your MySQL server."""
-""" lists all databases of your MySQL server."""
+--Write a script that lists all databases of your MySQL server.--
+-- lists all databases of your MySQL server.--
 SHOW DATABASES;
 
