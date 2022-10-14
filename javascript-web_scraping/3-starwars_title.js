4a77941a6a7ce299173738d9a79ba7a6da153176
Praise-Atadja <106454335+Praise-Atadja@users.noreply.github.com>
2022-10-14 22:40:31 +0200

    Update 3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/';
-let episode = process.argv[2];
-request(url + episode, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    let jsonobj = JSON.parse(body);
-    console.log(jsonobj.title);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
+
+const id = process.argv[2];
+request('https://swapi-api.hbtn.io/api/films/' + id, (error, res, body) => {
+  if (error) return console.log(error);
+  console.log(JSON.parse(body).title);
 });
