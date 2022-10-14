9f39427b6afc7f2c9c3cc78cd96ef6c14ddbce3e
Praise-Atadja <106454335+Praise-Atadja@users.noreply.github.com>
2022-10-14 21:56:51 +0200

    Update 6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 'use strict';
-const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const arg in args) {
-	  console.log(args[arg]);
+let args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let arg in args) {
+  console.log(args[arg]);
 }
