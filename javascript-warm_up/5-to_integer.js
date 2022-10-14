6ceeb5d21b4987017ebcc513181c92f5ee976887
Praise-Atadja <106454335+Praise-Atadja@users.noreply.github.com>
2022-10-14 21:55:47 +0200

    Update 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 'use strict';
-const arg = process.argv[2];
+let arg = process.argv[2];
 if (isNaN(arg)) {
-	  console.log('Not a number');
+  console.log('Not a number');
 } else {
-	  console.log('My number: ' + arg);
+  console.log('My number: ' + arg);
 }
