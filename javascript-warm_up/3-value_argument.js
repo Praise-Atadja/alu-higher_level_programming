2643f8f3a0e9aa438f2c2b0e733b62ab31a6443f
Praise-Atadja <106454335+Praise-Atadja@users.noreply.github.com>
2022-10-14 21:53:46 +0200

    Update 3-value_argument.js

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 'use strict';
-const arg = process.argv[2];
+let arg = process.argv[2];
 if (arg === undefined) {
-	  console.log('No argument');
+  console.log('No argument');
 } else {
-	  console.log(arg);
+  console.log(arg);
 }
