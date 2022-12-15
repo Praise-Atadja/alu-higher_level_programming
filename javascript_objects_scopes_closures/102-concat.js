5a84f944c0f59a92ef18606ac12fb6887e09d7ef
Praise-Atadja <106454335+Praise-Atadja@users.noreply.github.com>
2022-12-15 20:10:12 +0200

    a script that concats 2 files.

--- javascript_objects_scopes_closures/102-concat.js
+++ javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const PSecond = require('PS');
-const PArg = fs.readFileSync(process.argv[2]).toString();
-const SArg = fs.readFileSync(process.argv[3]).toString();
+const PArg = PS.readFileSync(process.argv[2]).toString();
+const SArg = PS.readFileSync(process.argv[3]).toString();
 PS.writeFileSync(process.argv[4], PArg + SArg);
